4add07ec630c428d7d3d21b260c619e59231b382
Hideo Yasumoto <hideo54@hideo54.com>
2023-04-03 15:18:34 +0900

    App が2つあったのをまとめた

--- functions/src/channelNotifier.ts
+++ functions/src/channelNotifier.ts
@@ -1,19 +1,11 @@
-import { App, ExpressReceiver } from '@slack/bolt';
+import type { App, ExpressReceiver } from '@slack/bolt';
 
-const receiver = new ExpressReceiver({
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    signingSecret: process.env.SLACK_SIGNING_SECRET!,
-    processBeforeResponse: true,
-});
-
-const app = new App({
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    token: process.env.SLACK_TOKEN!,
-    receiver,
-});
-
-const func = ({ channel }: { channel: string; }) => {
-    app.event('channel_created', async ({ event, client }) => {
+const func = ({ slackApp, receiver, channel }: {
+    slackApp: App;
+    receiver: ExpressReceiver,
+    channel: string;
+}) => {
+    slackApp.event('channel_created', async ({ event, client }) => {
         const { id, creator } = event.channel;
         client.chat.postMessage({
             channel,
@@ -22,7 +14,7 @@ const func = ({ channel }: { channel: string; }) => {
             text: `<@${creator}>が新しいチャンネル <#${id}> を作成しました :+1:`,
         });
     });
-    app.event('channel_unarchive', async ({ event, client }) => {
+    slackApp.event('channel_unarchive', async ({ event, client }) => {
         const { channel, user } = event;
         client.chat.postMessage({
             channel,
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import admin from 'firebase-admin'; // Default import required
 import { getFirestore } from 'firebase-admin/firestore';
-import { App } from '@slack/bolt';
+import { App, ExpressReceiver } from '@slack/bolt';
 import dotenv from 'dotenv';
 dotenv.config();
 import facultyNews from './facultyNews';
@@ -12,17 +12,24 @@ const randomChannel = process.env.SLACK_RANDOM_CHANNEL!;
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const facultyNewsChannel = process.env.SLACK_FACULTY_NEWS_CHANNEL!;
 
+const receiver = new ExpressReceiver({
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    signingSecret: process.env.SLACK_SIGNING_SECRET!,
+    processBeforeResponse: true,
+});
+const slackApp = new App({
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    token: process.env.SLACK_TOKEN!,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    signingSecret: process.env.SLACK_SIGNING_SECRET!,
+    receiver,
+});
+
 export const tmiSlackHourlyJob = functions
     .region('asia-northeast1')
     .pubsub.schedule('0 * * * *')
     .timeZone('Asia/Tokyo')
     .onRun(async () => {
-        const slackApp = new App({
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            token: process.env.SLACK_TOKEN!,
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            signingSecret: process.env.SLACK_SIGNING_SECRET!,
-        });
         admin.initializeApp();
         const firestoreDb = getFirestore();
         await facultyNews({ slackApp, firestoreDb, channel: facultyNewsChannel });
@@ -30,4 +37,10 @@ export const tmiSlackHourlyJob = functions
 
 export const tmiSlackEventsReceiver = functions
     .region('asia-northeast1')
-    .https.onRequest(channelNotifier({ channel: randomChannel }));
+    .https.onRequest(
+        channelNotifier({
+            slackApp,
+            receiver,
+            channel: randomChannel,
+        })
+    );
